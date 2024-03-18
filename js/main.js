4706f5d3781ae629d5023a45c97223119b2f50c8
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-03-18 15:43:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
     // input label
     const inputs = document.querySelectorAll('.active-on-type');
-    inputs.forEach(function(input) {
+    inputs.forEach(function (input) {
         input.addEventListener('input', function () {
             if (input.value.trim() !== '') {
                 input.classList.add('active');
@@ -30,67 +30,39 @@ $(document).ready(function () {
     });
 
 
+    // menu
 
-
-    // $("header.header nav ul").on("click", "a:not(.normallink)", function (event) {
-    //     event.preventDefault();
-    //     var id = $(this).attr('href'),
-    //         top = $(id).offset().top - 100;
-    //     $('body,html').animate({ scrollTop: top }, 800);
-    // });
-
-    // $("footer ul").on("click", "a:not(.normallink)", function (event) {
-    //     event.preventDefault();
-    //     var id = $(this).attr('href'),
-    //         top = $(id).offset().top - 100;
-    //     $('body,html').animate({ scrollTop: top }, 800);
-    // });
+    $('.menubtn').click(function () {
+        $(this).toggleClass('active');
+        $('.menuwrapper').toggleClass('show');
+        $('body').toggleClass('disscroll');
+    });
 
 
 
 
+    $("header.header nav ul").on("click", "a:not(.normallink)", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - 80;
+        $('body,html').animate({ scrollTop: top }, 800);
+    });
+
+    $("footer .footerboxlist ul").on("click", "a:not(.normallink)", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top - 80;
+        $('body,html').animate({ scrollTop: top }, 800);
+    });
 
 
+    // AOS init
+    AOS.init({
+        // disable: 'phone',
+        easing: 'ease-in-out',
+        duration: 800
+    });
 
-
-    // var menuBtn = document.querySelector('.menubtn');
-    // var mobMenu = document.querySelector('.header');
-    // var overlay = null;
-
-    // menuBtn.addEventListener('click', function () {
-    //     if (!overlay) {
-    //         overlay = document.createElement('div');
-    //         overlay.className = 'overlay';
-    //         mobMenu.appendChild(overlay);
-    //         setTimeout(function () {
-    //             overlay.classList.add('active');
-    //         }, 10);
-    //     }
-    // });
-
-    // var closeMenuBtn = document.querySelector('.closemenu');
-    // var popupCloseBtn = document.querySelector('.popup__close');
-
-    // closeMenuBtn.addEventListener('click', closeMenu);
-    // popupCloseBtn.addEventListener('click', closeMenu);
-
-    // function closeMenu() {
-    //     if (overlay) {
-    //         overlay.classList.remove('active');
-    //         setTimeout(function () {
-    //             mobMenu.removeChild(overlay);
-    //             overlay = null;
-    //         }, 300);
-    //     }
-    // }
-
-    // // Додаємо обробник подій для кожного пункту меню
-    // var menuItems = document.querySelectorAll('.header ul li a');
-    // menuItems.forEach(function (item) {
-    //     item.addEventListener('click', function () {
-    //         closeMenu();
-    //     });
-    // });
 
 
 
